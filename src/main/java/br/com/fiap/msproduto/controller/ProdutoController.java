865402abaf7c076199e244abfb951951e72cdcf0
brunoguerra <brunoguerra@hotmail.com>
2024-04-02 08:57:13 -0300

    feat: crud base completo

--- src/main/java/br/com/fiap/msproduto/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/msproduto/controller/ProdutoController.java
@@ -1,11 +1,11 @@
 package br.com.fiap.msproduto.controller;
 
+import br.com.fiap.msproduto.ProdutoApplication;
 import br.com.fiap.msproduto.model.Produto;
 import br.com.fiap.msproduto.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,30 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    @PostMapping
+    public Produto cadastrarProduto(@RequestBody Produto produto){
+        return produtoService.cadastrarProduto(produto);
+    }
+
     @GetMapping
     public List<Produto> listarProduto(){
         return produtoService.listarProduto();
     }
+
+    @GetMapping("/{produtoId}")
+    public ResponseEntity<?> listarUmProduto(@PathVariable Integer produtoId){
+        return produtoService.listarUmProduto(produtoId);
+    }
+
+    @PutMapping("/{produtoId}")
+    public Produto atualizarProduto(@PathVariable Integer produtoId,
+                                    @RequestBody Produto novoProuto){
+        return produtoService.atualizarProduto(produtoId, novoProuto);
+    }
+
+    @DeleteMapping("/{produtoId}")
+    public void excluirProduto(@PathVariable Integer produtoId){
+        produtoService.excluirProduto(produtoId);
+    }
+
 }
--- src/main/java/br/com/fiap/msproduto/service/ProdutoService.java
+++ src/main/java/br/com/fiap/msproduto/service/ProdutoService.java
@@ -1,11 +1,16 @@
 package br.com.fiap.msproduto.service;
 
+import br.com.fiap.msproduto.ProdutoApplication;
 import br.com.fiap.msproduto.model.Produto;
 import br.com.fiap.msproduto.repository.ProdutoRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ProdutoService {
@@ -14,5 +19,43 @@ public class ProdutoService {
     private ProdutoRepository produtoRepository;
     public List<Produto> listarProduto(){
         return produtoRepository.findAll();
+    }
+
+    public Produto cadastrarProduto(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public ResponseEntity<?> listarUmProduto(Integer produtoId){
+        Produto produto = produtoRepository.findById(produtoId).orElse(null);
+        if (produto != null){
+            return ResponseEntity.ok(produto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
+    }
+
+    public Produto atualizarProduto(Integer produtoId, Produto novoProduto){
+        Produto produtoExistente = produtoRepository.findById(produtoId).orElse(null);
+
+        if ( produtoExistente != null) {
+            produtoExistente.setDescricao(novoProduto.getDescricao());
+            produtoExistente.setNome(novoProduto.getNome());
+            produtoExistente.setQuantidade_estoque(novoProduto.getQuantidade_estoque());
+            produtoExistente.setPreco(novoProduto.getPreco());
+
+            return produtoRepository.save(produtoExistente);
+        } else {
+           throw new NoSuchElementException("Produto não encontrado");
+        }
+    }
+
+    public void  excluirProduto(Integer produtoId){
+        Produto produtoExistente = produtoRepository.findById(produtoId).orElse(null);
+
+        if ( produtoExistente != null) {
+            produtoRepository.delete(produtoExistente);
+        } else {
+            throw new NoSuchElementException("Produto não encontrado");
+        }
     }
 }
